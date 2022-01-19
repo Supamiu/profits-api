5936c18b80b6cc6bc52b7dd67d03de8a5e684ef1
Flavien Normand <contact@flavien-normand.fr>
2022-01-19 09:51:56 +0100

    fix(updater): fixed level requirements computing

--- src/api.ts
+++ src/api.ts
@@ -38,7 +38,9 @@ function getScore(item: any, selfSufficient = true): number {
                 && row.profit.c < 99999999
                 && row.complexity < maxComplexity
                 && row.v24 > minV24
-                && row.levelReqs.every((lvl: number, i: number) => levels[i] >= lvl);
+                && row.levelReqs.every((lvl: number, i: number) => {
+                    return (!selfSufficient && i > 8) || levels[i] >= lvl;
+                });
         }).sort((a, b) => {
             if (!selfSufficient) {
                 return getScore(b, false) - getScore(a, false);
--- src/common.ts
+++ src/common.ts
@@ -81,6 +81,9 @@ export async function computeProfit(item: Item, server: string, redis: RedisClie
 
 export function getLevelRequirements(item: Item, items: Record<number, Item>): number[] {
     let baseRequirements = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    if (item.id < 20) {
+        return baseRequirements;
+    }
     if (!item) {
         return baseRequirements;
     }
